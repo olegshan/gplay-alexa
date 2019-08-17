74afb3e46b6fc074078654a666bfc54dc552f541
olegshan <olegshan@gmail.com>
2019-08-17 19:53:21 +0300

    access level changed

--- src/main/java/com/olegshan/gplayalexa/speechlet/SpeechletConstants.java
+++ src/main/java/com/olegshan/gplayalexa/speechlet/SpeechletConstants.java
@@ -1,11 +1,11 @@
 package com.olegshan.gplayalexa.speechlet;
 
-public class SpeechletConstants {
-    public static final String WELCOME_TEXT            = "Welcome to Google Music skill.";
-    public static final String CHOOSE_THE_SONG_REQUEST = "Say 'play' and then name the artist and the song.";
-    public static final String WRONG_REQUEST           = "Sorry, I didn't get that.";
-    public static final String ERROR                   = "Sorry, something went wrong. Please try again.";
+class SpeechletConstants {
+    static final String WELCOME_TEXT            = "Welcome to Google Music skill.";
+    static final String CHOOSE_THE_SONG_REQUEST = "Say 'play' and then name the artist and the song.";
+    static final String WRONG_REQUEST           = "Sorry, I didn't get that.";
+    static final String ERROR                   = "Sorry, something went wrong. Please try again.";
 
-    public static final String GOOGLE_MUSIC_INTENT = "GoogleMusic";
-    public static final String SONG_SLOT           = "song";
+    static final String GOOGLE_MUSIC_INTENT = "GoogleMusic";
+    static final String SONG_SLOT           = "song";
 }
