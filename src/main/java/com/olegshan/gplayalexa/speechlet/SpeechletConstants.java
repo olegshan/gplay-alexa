ee27731a728457578d347aabcca23962edf5374b
olegshan <olegshan@gmail.com>
2019-09-16 23:00:14 +0300

    speech constant changed

--- src/main/java/com/olegshan/gplayalexa/speechlet/SpeechletConstants.java
+++ src/main/java/com/olegshan/gplayalexa/speechlet/SpeechletConstants.java
@@ -2,7 +2,7 @@ package com.olegshan.gplayalexa.speechlet;
 
 class SpeechletConstants {
     static final String WELCOME_TEXT             = "Welcome to Google Music skill.";
-    static final String CHOOSE_THE_MUSIC_REQUEST = "Say 'play' or 'play album' and then name the artist and the title.";
+    static final String CHOOSE_THE_MUSIC_REQUEST = "Say 'play' or 'get album' and then name the artist and the title.";
     static final String WRONG_REQUEST            = "Sorry, I didn't get that.";
     static final String ERROR                    = "Sorry, something went wrong. Please try again.";
 
